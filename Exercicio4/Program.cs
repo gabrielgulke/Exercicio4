49fccf9e517e209517809f3e1dad5edecb20a892
Gabriel Gulke <ggulke070@gmail.com>
2022-07-02 12:05:51 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 4 \nNúmeros de 1 a 50");
+
+            for (int i = 1; i <= 50; i++)
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
